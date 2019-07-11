272fd5e5aec6ae8ac9e46a9ca3b4dd5aca36e9d9
shradha001 <shradhamallik.dev@gmail.com>
2019-07-11 21:33:01 +0530

    Add the first homework

---
+++ homework_1/main.py
@@ -0,0 +1,35 @@
+"""
+Homework Assignment #1: Variables
+
+Listing the metadata of "What Hurts the Most" Song
+
+"""
+# song name
+song_name = "What Hurts the Most"
+
+# artist name
+artist = "Rascal Flatts"
+
+# album name
+album = "Me and My Gang"
+
+# recorded and released year
+recorded_year, released_year = 2005, 2006
+
+# song length in minutes
+duration_in_minutes = 3.33
+
+# genres
+genre = "country"
+
+# label
+label = "Lyric Street"
+
+print('My favourite song is', song_name)
+print('by', artist)
+print('from the album', album)
+print('It was recorded in the year', recorded_year)
+print('and released in', released_year, ".")
+print('The song is', duration_in_minutes, 'minutes long.')
+print('Genre is', genre)
+print('and  label is ', label)
