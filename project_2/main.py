3b9cce6a246e64a05b426e3e36c7517a5e00005d
shradha001 <shradhamallik.dev@gmail.com>
2019-07-27 11:44:18 +0530

    add color

--- project_2/main.py
+++ project_2/main.py
@@ -13,6 +13,8 @@ Project #2: Hangman
 
 """
  
+from termcolor import colored, cprint
+
 def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=False, left_leg=False, right_leg=False):
     for row in range(10):
         if row == 0:
@@ -31,11 +33,11 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
                 if column == 0:
                     print(" ",end="")
                 elif column == 1:
-                    print("_",end="")
+                    cprint("_",'red',end="")
                 elif column == 2:
                     print("|", end="")
                 elif column == 3:
-                    print("_", end="")
+                    cprint("_",'red', end="")
                 elif column == 19:
                     print("|")
                 else:
@@ -44,11 +46,11 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
         elif face == True and row == 2:
             for column in range(20):
                 if column == 0 or column == 4:
-                    print("|", end="")
+                    cprint("|",'red', end="")
                 elif column == 1 or column == 3: 
                     print(" ", end="")
                 elif column == 2:
-                    print("_", end="")
+                    cprint("_",'red', end="")
                 elif column == 19:
                     print("|")
                 else:
@@ -58,7 +60,7 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
         elif neck == True and row == 3:
             for column in range(20):
                 if column == 2:
-                    print("|", end="")
+                    cprint("|",'red', end="")
                 elif column == 19:
                     print("|")
                 else:
@@ -68,15 +70,15 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
         elif left_hand == True and row == 4:
             for column in range(20):
                 if column == 0:
-                    print("/", end="")
+                    cprint("/",'red', end="")
                 elif column == 1:
                     print(" ",end="")
                 elif column == 2:
-                    print("|", end="")
+                    cprint("|",'red', end="")
                 elif column == 3:
                     print(" ",end="")
                 elif right_hand == True and column == 4:
-                    print("\\", end="")
+                    cprint("\\",'red', end="")
                 elif column == 19:
                     print("|")
                 else:
@@ -86,7 +88,7 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
         elif body == True and row == 5:
             for column in range(20):
                 if column == 2:
-                    print("|", end="")
+                    cprint("|",'red', end="")
                 elif column == 19:
                     print("|")
                 else:
@@ -96,14 +98,14 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
         elif left_leg and row == 6:
             for column in range(20):
                 if column == 0:
-                    print("/", end="")
+                    cprint("/",'red', end="")
                 elif column == 1:
                     print(" ",end="")
                 elif column == 3:
                     print(" ",end="")
                 # right leg
                 elif right_leg and column == 4:
-                    print("\\", end="")
+                    cprint("\\",'red', end="")
                 elif column == 19:
                     print("|")
                 else:
@@ -126,15 +128,20 @@ def drawHangman(face=False, neck=False, body=False, left_hand=False, right_hand=
 
 
 # phrase should only be alphabets
-def askHostToSayPhrase():
-    word = input("What's your secret word?\n")
-    return word.lower()
+def askHostToSayPhrase(repeatText = None):
+    question = repeatText if repeatText is not None else "Type your secret word.\n"
+    word = input(question)
+    if type(word) == str and len(word) == 3:
+        return word.lower()
+    else:
+        return askHostToSayPhrase("Hey, please make it atleast three words! Type again.\n")
 
-
+#draw guessed letters or blanks
 def drawGuessedLetters(guessed_letters):
+    print("Word: ", end="")
     for letter in guessed_letters:
         print(letter, end="  ")
-    print("\n\n")
+    print("\n")
 
 # checks if the letter is guessed is right or not
 def checkGuess(letter, phrase_letters):
@@ -142,16 +149,29 @@ def checkGuess(letter, phrase_letters):
     if letter in phrase_letters:
         guess = True
     return guess
-        
 
+# fill guessed letters      
 def fillBlanks(letter, phrase_letters, guessed_letters):
     indices = [i for i, el in enumerate(phrase_letters) if el == letter]
     for index in indices:
         guessed_letters[index] = letter
+    return guessed_letters
 
-def guessLetter():
-    word = input("What's your guess?\n")
-    return word.lower()
+def guessLetter(repeatText = None):
+    question = repeatText if repeatText is not None else "What's your guess?\n"
+    word = input(question)
+    if type(word) == str and len(word) == 1:
+        return word.lower()
+    else:
+        return guessLetter("Hey, one letter at a time! Type again.\n")
+    
+
+
+def showMisses(misses):
+    print("Misses: ", end="")
+    for miss in misses:
+        print(miss, end=",")
+    print("\n")
 
 
 def drawHangmanBasedOnWrongGuessed(committed_mistakes):
@@ -172,6 +192,7 @@ def drawHangmanBasedOnWrongGuessed(committed_mistakes):
     if committed_mistakes == 7:
         drawHangman(True, True, True, True, True, True, True)
 
+
 def startHangman():
     #ask player to guess
     phrase = askHostToSayPhrase()
@@ -179,32 +200,29 @@ def startHangman():
     guessed_letters = ["__" for el in phrase_letters ]
     committed_mistakes = 0
     already_guessed_letters = []
+    missed_letters = []
 
-    print('phrase ',phrase)
-    print('phrase list', phrase_letters)
-    print('guessed list', guessed_letters)
+    # hide the secret word, clear the screen.
+    print(chr(27) + "[2J") 
 
     no_win = True
-
-    # hide the secret word
 
     # ask player to start guessing, while loop to continue
     # until the player guessed it correct or
     # the mistakes are 7
     while(committed_mistakes < 7 and no_win):
-        # draw hangman based on wrong guessed
-        drawHangmanBasedOnWrongGuessed(committed_mistakes)
-
         # draw guessed letters or blanks
         drawGuessedLetters(guessed_letters)
 
+        if len(missed_letters) > 0:
+            showMisses(missed_letters)
         # ask player to guess
         guessed_letter = guessLetter()
         
         
         # guessed letter should not be allowed
         if guessed_letter in already_guessed_letters:
-            print('Hey, you already guessed it, try something else!')
+            cprint("Hey, you already guessed it, try something else!", 'blue', attrs=['bold'])
             continue
         else:
             already_guessed_letters.append(guessed_letter)
@@ -214,9 +232,9 @@ def startHangman():
         # if guesed correctly, fill the blanks with the letter
         # inform the player it's the right guess
         if guessed == True:
-            fillBlanks(guessed_letter, phrase_letters, guessed_letters)
+            guessed_letters = fillBlanks(guessed_letter, phrase_letters, guessed_letters)
             # todo: make a list of appreciation and pick randomly
-            print("That's like a pro!")
+            cprint("That's like a pro!", 'magenta', attrs=['bold'])
 
             #check if the word is completed
             if guessed_letters == phrase_letters:
@@ -227,21 +245,23 @@ def startHangman():
         # inform the player it's a wrongguess
         else:
             committed_mistakes += 1
+            missed_letters.append(guessed_letter)
+            # draw hangman based on wrong guessed
+            drawHangmanBasedOnWrongGuessed(committed_mistakes)
             # todo: make a list of wrong guessed messages and pick randomly
-            print("Oops, that's not correct!")
+            cprint("Damn, you missed!", 'red', attrs=['bold'])
+
+        print("----------------------------------------------------------------------------")
     
     if no_win == False:
-        print("You won!")
+            cprint("You Won! Great Job!", 'cyan', attrs=['bold'])
     
     if committed_mistakes == 7:
         drawHangmanBasedOnWrongGuessed(committed_mistakes)
-        print("You lost!")
+        cprint("Oops, you lost. Better luck next time!", 'red', attrs=['bold'])
 
         
 
 
-
-
-
-
 startHangman()
+
